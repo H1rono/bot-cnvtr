d9f31de63c08efee21bcbb9fca754ea426ffcdb5
H1rono <hronok66@gmail.com>
2023-07-16 16:30:58 +0900

    :sparkles: Add database migration

---
+++ migrations/0_init.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS `webhooks` (
+    `id` CHAR(64) NOT NULL PRIMARY KEY,
+    `channel_id` CHAR(36) NOT NULL,
+    `owner_id` char(36) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS `owners` (
+    `id` CHAR(36) NOT NULL PRIMARY KEY,
+    `name` VARCHAR(30) NOT NULL,
+    `group` BOOLEAN NOT NULL DEFAULT FALSE,
+);
+
+CREATE TABLE IF NOT EXISTS `groups` (
+    `id` CHAR(36) NOT NULL PRIMARY KEY,
+    `name` VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` CHAR(36) NOT NULL PRIMARY KEY,
+    `name` VARCHAR(32) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS `group_members` (
+    `group_id` CHAR(36) NOT NULL,
+    `user_id` CHAR(36) NOT NULL,
+    PRIMARY KEY (`group_id`, `user_id`),
+    FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+);
