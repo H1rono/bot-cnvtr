ebec48fa2c978ced44c93333e7ce99d2f7a9149b
H1rono <hronok66@gmail.com>
2023-07-16 19:05:53 +0900

    :bug: Fix SQL syntax errors

--- migrations/0_init.sql
+++ migrations/0_init.sql
@@ -1,23 +1,23 @@
 CREATE TABLE IF NOT EXISTS `webhooks` (
     `id` CHAR(64) NOT NULL PRIMARY KEY,
     `channel_id` CHAR(36) NOT NULL,
-    `owner_id` char(36) NOT NULL,
+    `owner_id` CHAR(36) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `owners` (
     `id` CHAR(36) NOT NULL PRIMARY KEY,
     `name` VARCHAR(30) NOT NULL,
-    `group` BOOLEAN NOT NULL DEFAULT FALSE,
+    `group` BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS `groups` (
     `id` CHAR(36) NOT NULL PRIMARY KEY,
-    `name` VARCHAR(30) NOT NULL,
+    `name` VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `users` (
     `id` CHAR(36) NOT NULL PRIMARY KEY,
-    `name` VARCHAR(32) NOT NULL,
+    `name` VARCHAR(32) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `group_members` (
@@ -25,5 +25,5 @@ CREATE TABLE IF NOT EXISTS `group_members` (
     `user_id` CHAR(36) NOT NULL,
     PRIMARY KEY (`group_id`, `user_id`),
     FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 );
