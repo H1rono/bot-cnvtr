abed32ce25b505d66e147435835540f88cabd948
H1rono <hronok66@gmail.com>
2023-07-16 22:52:18 +0900

    :recycle: s/client/config/g

--- src/bot.rs
+++ src/bot.rs
@@ -9,21 +9,21 @@ use super::{config::BotConfig, Database};
 pub struct Bot {
     pub id: String,
     pub user_id: String,
-    pub client: Configuration,
+    pub config: Configuration,
 }
 
 impl Bot {
     pub fn new(id: &str, user_id: &str, access_token: &str) -> Self {
         let id = id.to_string();
         let user_id = user_id.to_string();
-        let client = Configuration {
+        let config = Configuration {
             bearer_access_token: Some(access_token.to_string()),
             ..Default::default()
         };
         Self {
             id,
             user_id,
-            client,
+            config,
         }
     }
 
@@ -34,14 +34,14 @@ impl Bot {
             bot_access_token,
             ..
         } = bot_config;
-        let client = Configuration {
+        let config = Configuration {
             bearer_access_token: Some(bot_access_token),
             ..Default::default()
         };
         Self {
             id: bot_id,
             user_id: bot_user_id,
-            client,
+            config,
         }
     }
 
