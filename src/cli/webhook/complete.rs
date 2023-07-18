92ea8eaa075200ba41ef76ef8563f7a32d7ad7a2
H1rono <hronok66@gmail.com>
2023-07-19 03:42:01 +0900

    :sparkles: Implement Completed

--- src/cli/webhook/complete.rs
+++ src/cli/webhook/complete.rs
@@ -1,7 +1,8 @@
 use traq_bot_http::payloads::{types::Message, DirectMessageCreatedPayload, MessageCreatedPayload};
 use uuid::Uuid;
 
-use crate::model::Owner;
+use super::incomplete;
+use crate::{cli::Completed, model::Owner};
 
 #[derive(Debug, Clone)]
 pub enum Webhook {
@@ -15,6 +16,17 @@ pub struct WebhookCreate {
     pub channel_name: Option<String>,
     pub channel_id: Uuid,
     pub owner: Owner,
+}
+
+impl Completed for WebhookCreate {
+    type Incomplete = incomplete::WebhookCreate;
+
+    fn incomplete(&self) -> Self::Incomplete {
+        incomplete::WebhookCreate {
+            channel: self.channel_name.clone(),
+            owner: Some(self.owner.name.clone()),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -42,7 +54,25 @@ impl From<DirectMessageCreatedPayload> for WebhookList {
     }
 }
 
+impl Completed for WebhookList {
+    type Incomplete = incomplete::WebhookList;
+
+    fn incomplete(&self) -> Self::Incomplete {
+        incomplete::WebhookList
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct WebhookDelete {
     pub webhook_id: String,
+}
+
+impl Completed for WebhookDelete {
+    type Incomplete = incomplete::WebhookDelete;
+
+    fn incomplete(&self) -> Self::Incomplete {
+        incomplete::WebhookDelete {
+            id: self.webhook_id.clone(),
+        }
+    }
 }
