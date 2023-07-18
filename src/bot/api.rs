3e9b60d4b581c8e76a45963330d16feddaae85a1
H1rono <hronok66@gmail.com>
2023-07-19 05:52:21 +0900

    :sparkles: Add send dm api in Bot

--- src/bot/api.rs
+++ src/bot/api.rs
@@ -2,6 +2,7 @@ use indoc::indoc;
 use uuid::Uuid;
 
 use traq::apis::message_api::post_message;
+use traq::apis::user_api::post_direct_message;
 use traq::models::{Message, PostMessageRequest};
 
 use super::{Bot, Result};
@@ -32,5 +33,32 @@ impl Bot {
             lang, code
         );
         self.send_message(channel_id, &message, false).await
+    }
+
+    pub async fn send_direct_message(
+        &self,
+        user_id: &Uuid,
+        content: &str,
+        embed: bool,
+    ) -> Result<Message> {
+        let req = PostMessageRequest {
+            content: content.to_string(),
+            embed: Some(embed),
+        };
+        let user_id = user_id.to_string();
+        let res = post_direct_message(&self.config, &user_id, Some(req)).await?;
+        Ok(res)
+    }
+
+    pub async fn send_code_dm(&self, user_id: &Uuid, lang: &str, code: &str) -> Result<Message> {
+        let message = format!(
+            indoc! {r#"
+            ```{}
+            {}
+            ```
+        "#},
+            lang, code
+        );
+        self.send_direct_message(user_id, &message, false).await
     }
 }
