004de3973f748b24731c1e9b6f4cdde48a2b1229
H1rono <hronok66@gmail.com>
2023-07-22 08:07:27 +0900

    :sparkles: Add sudo webhook command handler

--- src/bot/messages/cmd_sudo.rs
+++ src/bot/messages/cmd_sudo.rs
@@ -1,17 +1,31 @@
 use super::{Bot, Result};
 
-use crate::cli::sudo::SudoCompleted;
+use crate::cli::sudo::{
+    webhook::{Completed, Delete, ListAll},
+    SudoCompleted,
+};
 use crate::Database;
 
 impl Bot {
     pub(super) async fn handle_sudo_command(
         &self,
         sudo: SudoCompleted,
-        _db: &Database,
+        db: &Database,
     ) -> Result<()> {
         use SudoCompleted::*;
         match sudo {
-            Webhook(_wh) => Ok(()),
+            Webhook(Completed::ListAll(list_all)) => {
+                self.handle_sudo_wh_list_all(list_all, db).await
+            }
+            Webhook(Completed::Delete(delete)) => self.handle_sudo_wh_delete(delete, db).await,
         }
+    }
+
+    async fn handle_sudo_wh_list_all(&self, _list_all: ListAll, _db: &Database) -> Result<()> {
+        Ok(())
+    }
+
+    async fn handle_sudo_wh_delete(&self, _delete: Delete, _db: &Database) -> Result<()> {
+        Ok(())
     }
 }
