5d91c7f9b23d7351aad5a079892968460432bf0b
H1rono <hronok66@gmail.com>
2023-07-21 13:39:52 +0900

    :sparkles: Send dummy message on webhook POST

--- src/router/wh.rs
+++ src/router/wh.rs
@@ -3,6 +3,7 @@ use axum::{
     http::StatusCode,
     Json,
 };
+use indoc::indoc;
 use serde_json::Value;
 
 use super::{AppState, Error, Result};
@@ -22,30 +23,60 @@ pub(super) async fn get_wh(
 
 /// POST /wh/:id/github
 pub(super) async fn wh_github(
-    State(_st): State<AppState>,
-    Path(_id): Path<String>,
+    State(st): State<AppState>,
+    Path(id): Path<String>,
     Json(_payload): Json<Value>,
 ) -> Result<StatusCode> {
-    // TODO
+    let webhook = st.db.find_webhook(&id).await?.ok_or(Error::NotFound)?;
+    let message = indoc! {
+        r##"
+            GitHubからWebhookが送信されました。
+            実装は現在工事中です :construction:
+        "##
+    };
+    st.bot
+        .send_message(&webhook.channel_id, message, false)
+        .await
+        .map_err(Error::from)?;
     Ok(StatusCode::NOT_IMPLEMENTED)
 }
 
 /// POST /wh/:id/gitea
 pub(super) async fn wh_gitea(
-    State(_st): State<AppState>,
-    Path(_id): Path<String>,
+    State(st): State<AppState>,
+    Path(id): Path<String>,
     Json(_payload): Json<Value>,
 ) -> Result<StatusCode> {
-    // TODO
+    let webhook = st.db.find_webhook(&id).await?.ok_or(Error::NotFound)?;
+    let message = indoc! {
+        r##"
+            GiteaからWebhookが送信されました。
+            実装は現在工事中です :construction:
+        "##
+    };
+    st.bot
+        .send_message(&webhook.channel_id, message, false)
+        .await
+        .map_err(Error::from)?;
     Ok(StatusCode::NOT_IMPLEMENTED)
 }
 
 /// POST /wh/:id/clickup
 pub(super) async fn wh_clickup(
-    State(_st): State<AppState>,
-    Path(_id): Path<String>,
+    State(st): State<AppState>,
+    Path(id): Path<String>,
     Json(_payload): Json<Value>,
 ) -> Result<StatusCode> {
-    // TODO
+    let webhook = st.db.find_webhook(&id).await?.ok_or(Error::NotFound)?;
+    let message = indoc! {
+        r##"
+            ClickUpからWebhookが送信されました。
+            実装は現在工事中です :construction:
+        "##
+    };
+    st.bot
+        .send_message(&webhook.channel_id, message, false)
+        .await
+        .map_err(Error::from)?;
     Ok(StatusCode::NOT_IMPLEMENTED)
 }
