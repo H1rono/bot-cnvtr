99e90ae12a0affd0ddc5864577521c3310f3a20d
H1rono <hronok66@gmail.com>
2023-07-23 09:51:57 +0900

    :sparkles: Implement on_message_created handler

--- bot/src/messages.rs
+++ bot/src/messages.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 
-use traq_bot_http::payloads::{DirectMessageCreatedPayload, MessageCreatedPayload};
+use traq_bot_http::payloads::{types::Message, DirectMessageCreatedPayload, MessageCreatedPayload};
 
 use cli::{Cli, CompletedCmds, Incomplete};
 use model::Database;
@@ -21,16 +21,40 @@ fn parse_command(cmd: &str) -> Result<Cli, clap::Error> {
 }
 
 impl Bot {
+    async fn parse(&self, message: &Message) -> Result<Option<Cli>> {
+        match parse_command(&message.plain_text) {
+            Ok(c) => Ok(Some(c)),
+            Err(e) => {
+                self.send_code(&message.channel_id, "", &e.to_string())
+                    .await?;
+                Ok(None)
+            }
+        }
+    }
+
+    async fn run_command(&self, cmd: CompletedCmds, db: &Database) -> Result<()> {
+        use CompletedCmds::*;
+        match cmd {
+            Webhook(w) => self.handle_webhook_command(w, db).await,
+            Sudo(s) => self.handle_sudo_command(s, db).await,
+        }
+    }
+
     pub async fn on_message_created(
         &self,
         payload: MessageCreatedPayload,
-        _db: &Database,
+        db: &Database,
     ) -> Result<()> {
         print!(
             "{}さんがメッセージを投稿しました。\n内容: {}\n",
             payload.message.user.display_name, payload.message.text
         );
-        Ok(())
+        let cli = match self.parse(&payload.message).await? {
+            Some(c) => c,
+            None => return Ok(()),
+        };
+        let cmd = cli.cmd.complete(&payload);
+        self.run_command(cmd, db).await
     }
 
     pub async fn on_direct_message_created(
@@ -42,20 +66,11 @@ impl Bot {
             "{}さんがダイレクトメッセージを投稿しました。\n内容: {}\n",
             payload.message.user.display_name, payload.message.text
         );
-        let msg = &payload.message.plain_text;
-        let cid = payload.message.channel_id;
-        let cli = match parse_command(msg) {
-            Ok(c) => c,
-            Err(e) => {
-                self.send_code(&cid, "", &e.to_string()).await?;
-                return Ok(());
-            }
+        let cli = match self.parse(&payload.message).await? {
+            Some(c) => c,
+            None => return Ok(()),
         };
         let cmd = cli.cmd.complete(&payload);
-        match cmd {
-            CompletedCmds::Webhook(w) => self.handle_webhook_command(w, db).await,
-            CompletedCmds::Sudo(sudo) => self.handle_sudo_command(sudo, db).await,
-        }?;
-        Ok(())
+        self.run_command(cmd, db).await
     }
 }
