a214c2514d7c8c7b1511a2698e08224aed320ef8
H1rono <hronok66@gmail.com>
2023-07-23 21:19:16 +0900

    :sparkles: Implement `sudo webhook delete`

--- bot/src/messages/cmd_sudo.rs
+++ bot/src/messages/cmd_sudo.rs
@@ -4,6 +4,7 @@ use cli::sudo::{
     webhook::{Completed, Delete, ListAll},
     SudoCompleted,
 };
+use futures::{pin_mut, StreamExt};
 use model::Database;
 
 impl Bot {
@@ -33,11 +34,41 @@ impl Bot {
         Ok(())
     }
 
-    async fn handle_sudo_wh_delete(&self, delete: Delete, _db: &Database) -> Result<()> {
+    async fn handle_sudo_wh_delete(&self, delete: Delete, db: &Database) -> Result<()> {
         if !delete.valid {
             let message = "Permission denied.";
             self.send_code(&delete.talking_channel_id, "", message)
                 .await?;
+        }
+        let webhook = match db.find_webhook(&delete.id).await? {
+            Some(w) => w,
+            None => {
+                let message = format!("エラー: webhook {} は存在しません", delete.id);
+                self.send_message(&delete.talking_channel_id, &message, false)
+                    .await?;
+                return Ok(());
+            }
+        };
+        let owner = db.find_owner(&webhook.owner_id).await?.unwrap();
+        let own_users = if owner.group {
+            self.get_group_members(&owner.id)
+                .await?
+                .into_iter()
+                .map(|gm| gm.id)
+                .collect::<Vec<_>>()
+        } else {
+            vec![owner.id]
+        };
+        db.delete_webhook(&webhook.id).await?;
+        let it = async_stream::stream! {
+            let message = format!("Webhook {} を削除しました", delete.id);
+            for u in own_users {
+                yield self.send_direct_message(&u, &message, false).await;
+            }
+        };
+        pin_mut!(it);
+        while let Some(r) = it.next().await {
+            r?;
         }
         Ok(())
     }
