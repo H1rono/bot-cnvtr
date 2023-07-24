7da3e34d7f4f3cda1ef87b240c57be37d82fc0e4
H1rono <hronok66@gmail.com>
2023-07-24 13:37:08 +0900

    :construction: Show event type in clickup webhook

--- router/src/wh/clickup.rs
+++ router/src/wh/clickup.rs
@@ -1,13 +1,22 @@
 use axum::http::HeaderMap;
-use indoc::indoc;
+use indoc::formatdoc;
 use serde_json::Value;
 
-use crate::Result;
+use crate::{Error, Result};
 
-pub(super) fn handle(_headers: HeaderMap, _payload: Value) -> Result<String> {
-    let message = indoc! {r#"
-        ClickUpからWebhookが送信されました。
-        実装は現在工事中です :construction:
-    "#};
-    Ok(message.to_string())
+pub(super) fn handle(_headers: HeaderMap, payload: Value) -> Result<String> {
+    let event = payload
+        .get("event")
+        .ok_or(Error::BadRequest)?
+        .as_str()
+        .ok_or(Error::BadRequest)?;
+    let message = formatdoc! {
+        r#"
+            ClickUpからWebhookが送信されました。
+            イベント: {}
+            実装は現在工事中です :construction:
+        "#,
+        event
+    };
+    Ok(message)
 }
