af590323fe7559b1eb482f9ece2d7a62f29123d2
H1rono <hronok66@gmail.com>
2023-07-24 16:22:41 +0900

    :sparkles: Add ValueExt methods

--- router/src/wh/utils.rs
+++ router/src/wh/utils.rs
@@ -8,6 +8,10 @@ pub(crate) trait ValueExt {
         I: Index;
     fn as_str_or_err(&self) -> Result<&str>;
     fn as_array_or_err(&self) -> Result<&Vec<Value>>;
+    fn as_u64_or_err(&self) -> Result<u64>;
+    fn as_i64_or_err(&self) -> Result<i64>;
+    fn as_f64_or_err(&self) -> Result<f64>;
+    fn as_bool_or_err(&self) -> Result<bool>;
 }
 
 impl ValueExt for Value {
@@ -24,5 +28,21 @@ impl ValueExt for Value {
 
     fn as_array_or_err(&self) -> Result<&Vec<Value>> {
         self.as_array().ok_or(Error::BadRequest)
+    }
+
+    fn as_u64_or_err(&self) -> Result<u64> {
+        self.as_u64().ok_or(Error::BadRequest)
+    }
+
+    fn as_i64_or_err(&self) -> Result<i64> {
+        self.as_i64().ok_or(Error::BadRequest)
+    }
+
+    fn as_f64_or_err(&self) -> Result<f64> {
+        self.as_f64().ok_or(Error::BadRequest)
+    }
+
+    fn as_bool_or_err(&self) -> Result<bool> {
+        self.as_bool().ok_or(Error::BadRequest)
     }
 }
