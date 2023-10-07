dd1a798e510bab88c05998bf75807122ce32c9a8
H1rono <hronok66@gmail.com>
2023-10-07 19:58:19 +0900

    :fire: Remove `Channel` from `entity`

--- entity/src/lib.rs
+++ entity/src/lib.rs
@@ -1,10 +1,8 @@
-mod channel;
 mod group;
 mod owner;
 mod user;
 mod webhook;
 
-pub use channel::Channel;
 pub use group::Group;
 pub use owner::{Owner, OwnerKind};
 pub use user::User;
--- entity/src/webhook.rs
+++ entity/src/webhook.rs
@@ -1,17 +1,21 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use crate::{Channel, Owner};
+use crate::Owner;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Webhook {
     pub id: Uuid,
-    pub channel: Channel,
+    pub channel_id: Uuid,
     pub owner: Owner,
 }
 
 impl Webhook {
-    pub fn new(id: Uuid, channel: Channel, owner: Owner) -> Self {
-        Self { id, channel, owner }
+    pub fn new(id: Uuid, channel_id: Uuid, owner: Owner) -> Self {
+        Self {
+            id,
+            channel_id,
+            owner,
+        }
     }
 }
