023deb3efe25235c304b0754da503ba598cda607
H1rono <hronok66@gmail.com>
2023-12-27 15:26:54 +0900

    :wrench: Use crane in flake.nix

--- flake.nix
+++ flake.nix
@@ -4,58 +4,94 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix/monthly";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crane = {
+      url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, flake-utils, fenix, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        toolchain = fenix.packages.${system}.fromToolchainFile {
-          file = ./rust-toolchain.toml;
-          sha256 = "sha256-U2yfueFohJHjif7anmJB5vZbpP7G6bICH4ZsjtufRoU=";
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
-        rustPlatform = pkgs.makeRustPlatform {
-          rustc = toolchain;
-          cargo = toolchain;
-        };
-        nativeBuildInputs = with pkgs; [ pkg-config ];
-        buildInputs = with pkgs; [ openssl libiconv ]
-          ++ lib.optionals stdenvNoCC.isDarwin [ darwin.Security darwin.apple_sdk.frameworks.SystemConfiguration ];
-        defaultBuildArgs = {
-          pname = "cnvtr";
-          version = "0.1.0";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-          inherit nativeBuildInputs buildInputs;
-          doCheck = false;
-          buildType = "debug";
-        };
-        buildRustPackage = attrs: rustPlatform.buildRustPackage (defaultBuildArgs // attrs);
+        inherit (pkgs) lib;
+
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          # Common arguments can be set here to avoid repeating them later
+          buildInputs = with pkgs; [
+            # Add additional build inputs here
+            openssl
+          ] ++ lib.optionals stdenvNoCC.isDarwin [
+            # Additional darwin specific inputs can be set here
+            libiconv
+            darwin.Security
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
+        } // (craneLib.crateNameFromCargoToml { cargoToml = ./cnvtr/Cargo.toml; });
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        build = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
       in
       {
-        devShells.default = pkgs.stdenv.mkDerivation {
-          name = "bot-cnvtr";
-          nativeBuildInputs = with pkgs; nativeBuildInputs ++ [ toolchain cargo-make sqlx-cli grcov ];
-          inherit buildInputs;
+        checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit build;
+
+          # Run clippy (and deny all warnings) on the crate source,
+          # again, resuing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+          doc = craneLib.cargoDoc (commonArgs // {
+            inherit cargoArtifacts;
+          });
+
+          # Check formatting
+          fmt = craneLib.cargoFmt commonArgs;
         };
-        packages = {
-          default = buildRustPackage { };
-          with-check = buildRustPackage {
-            checkPhase = ''
-              cargo fmt --all -- --check
-              cargo clippy --all-targets --all-features -- -D warnings
-              cargo test --no-run
-            '';
-            doCheck = true;
-            installPhase = ''
-              mkdir -p $out
-              mv ./target/* $out/
-            '';
-          };
+        packages.default = build;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = build;
+        };
+
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = [
+            # pkgs.ripgrep
+          ];
         };
       }
     );
