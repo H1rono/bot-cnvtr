846494311799e4be0c57304898c3843b6dfcf643
H1rono <hronok66@gmail.com>
2023-12-29 17:22:16 +0900

    :sparkles: impl `Bot<I>` for `BotImpl`

--- usecases/src/bot.rs
+++ usecases/src/bot.rs
@@ -2,16 +2,18 @@ use std::future::Future;
 
 use traq_bot_http::Event;
 
+use domain::Infra;
+
 use crate::config::Config;
 use crate::error::{Error, Result};
 
-pub trait Bot {
+pub trait Bot<I: Infra>: Send + Sync + 'static {
     type Error: Send + Sync + 'static;
 
-    fn handle_event<I: domain::Infra>(
+    fn handle_event(
         &self,
         infra: &I,
-        event: traq_bot_http::Event,
+        event: Event,
     ) -> impl Future<Output = Result<(), Self::Error>> + Send;
 }
 
@@ -55,6 +57,36 @@ impl BotImpl {
             MessageCreated(payload) => self.on_message_created(repo, client, payload).await,
             DirectMessageCreated(payload) => {
                 self.on_direct_message_created(repo, client, payload).await
+            }
+            _ => Ok(()),
+        }
+    }
+}
+
+impl<I: Infra> Bot<I> for BotImpl
+where
+    crate::Error: From<I::Error>,
+{
+    type Error = crate::Error;
+
+    async fn handle_event(&self, infra: &I, event: Event) -> Result<(), Self::Error> {
+        use Event::*;
+        match event {
+            Joined(payload) => {
+                self.on_joined(infra.repo(), infra.traq_client(), payload)
+                    .await
+            }
+            Left(payload) => {
+                self.on_left(infra.repo(), infra.traq_client(), payload)
+                    .await
+            }
+            MessageCreated(payload) => {
+                self.on_message_created(infra.repo(), infra.traq_client(), payload)
+                    .await
+            }
+            DirectMessageCreated(payload) => {
+                self.on_direct_message_created(infra.repo(), infra.traq_client(), payload)
+                    .await
             }
             _ => Ok(()),
         }
