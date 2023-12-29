e728897bcea88212b0d62b95107a5574008e3055
H1rono <hronok66@gmail.com>
2023-12-29 15:43:56 +0900

    :recycle: Move place of `BotImpl`

--- usecases/src/bot.rs
+++ usecases/src/bot.rs
@@ -1,5 +1,10 @@
 use std::future::Future;
 
+use traq_bot_http::Event;
+
+use crate::config::Config;
+use crate::error::{Error, Result};
+
 pub trait Bot {
     type Error: Send + Sync + 'static;
 
@@ -8,4 +13,50 @@ pub trait Bot {
         infra: &I,
         event: traq_bot_http::Event,
     ) -> impl Future<Output = Result<(), Self::Error>> + Send;
+}
+
+#[derive(Debug, Clone)]
+pub struct BotImpl {
+    pub id: String,
+    pub user_id: String,
+}
+
+impl BotImpl {
+    pub fn new(id: &str, user_id: &str) -> Self {
+        let id = id.to_string();
+        let user_id = user_id.to_string();
+        Self { id, user_id }
+    }
+
+    pub fn from_config(bot_config: Config) -> Self {
+        let Config {
+            bot_id,
+            bot_user_id,
+        } = bot_config;
+        Self {
+            id: bot_id,
+            user_id: bot_user_id,
+        }
+    }
+
+    pub async fn handle_event<E1, E2>(
+        &self,
+        repo: &impl domain::Repository<Error = E1>,
+        client: &impl domain::TraqClient<Error = E2>,
+        event: Event,
+    ) -> Result<(), Error>
+    where
+        Error: From<E1> + From<E2>,
+    {
+        use Event::*;
+        match event {
+            Joined(payload) => self.on_joined(repo, client, payload).await,
+            Left(payload) => self.on_left(repo, client, payload).await,
+            MessageCreated(payload) => self.on_message_created(repo, client, payload).await,
+            DirectMessageCreated(payload) => {
+                self.on_direct_message_created(repo, client, payload).await
+            }
+            _ => Ok(()),
+        }
+    }
 }
--- usecases/src/lib.rs
+++ usecases/src/lib.rs
@@ -1,5 +1,3 @@
-use traq_bot_http::Event;
-
 mod bot;
 pub(crate) mod cli;
 mod config;
@@ -8,53 +6,7 @@ mod messages;
 mod system;
 mod wh_handler;
 
-pub use bot::Bot;
+pub use bot::{Bot, BotImpl};
 pub use config::Config;
 pub use error::{Error, Result};
 pub use wh_handler::WebhookHandler;
-
-#[derive(Debug, Clone)]
-pub struct BotImpl {
-    pub id: String,
-    pub user_id: String,
-}
-
-impl BotImpl {
-    pub fn new(id: &str, user_id: &str) -> Self {
-        let id = id.to_string();
-        let user_id = user_id.to_string();
-        Self { id, user_id }
-    }
-
-    pub fn from_config(bot_config: Config) -> Self {
-        let Config {
-            bot_id,
-            bot_user_id,
-        } = bot_config;
-        Self {
-            id: bot_id,
-            user_id: bot_user_id,
-        }
-    }
-
-    pub async fn handle_event<E1, E2>(
-        &self,
-        repo: &impl domain::Repository<Error = E1>,
-        client: &impl domain::TraqClient<Error = E2>,
-        event: Event,
-    ) -> Result<(), Error>
-    where
-        Error: From<E1> + From<E2>,
-    {
-        use Event::*;
-        match event {
-            Joined(payload) => self.on_joined(repo, client, payload).await,
-            Left(payload) => self.on_left(repo, client, payload).await,
-            MessageCreated(payload) => self.on_message_created(repo, client, payload).await,
-            DirectMessageCreated(payload) => {
-                self.on_direct_message_created(repo, client, payload).await
-            }
-            _ => Ok(()),
-        }
-    }
-}
