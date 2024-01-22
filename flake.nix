8f43baf0429bdfe942ba9a8f4a9dd281c546726e
H1rono <hronok66@gmail.com>
2024-01-23 01:55:35 +0900

    :wrench: Add golang in devshell

--- flake.nix
+++ flake.nix
@@ -99,12 +99,18 @@
           # Inherit inputs from checks.
           checks = self.checks.${system};
 
+          shellHook = ''
+            unset GOROOT
+            unset GOPATH
+            unset GOTOOLDIR
+          '';
+
           # Additional dev-shell environment variables can be set directly
           # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
-            # pkgs.ripgrep
+            pkgs.go_1_21
           ];
         };
       }
