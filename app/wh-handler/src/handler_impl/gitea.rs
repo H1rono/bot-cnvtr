f03907f229c6d16c9e03a87a2830c27ab10f219f
H1rono <hronok66@gmail.com>
2024-01-23 03:59:17 +0900

    :pencil: Memo

--- app/wh-handler/src/handler_impl/gitea.rs
+++ app/wh-handler/src/handler_impl/gitea.rs
@@ -8,5 +8,7 @@ pub(super) fn handle(_headers: HeaderMap, _payload: &str) -> Result<Option<Strin
         GiteaからWebhookが送信されました。
         実装は現在工事中です :construction:
     "#};
+    // https://github.com/traPtitech/gitea/blob/8abe54a9d4db1fdce7c517dc500a51e77d1f2c16/services/webhook/deliver.go#L124-L138
+    // https://github.com/traPtitech/gitea/blob/8abe54a9d4db1fdce7c517dc500a51e77d1f2c16/modules/webhook/type.go#L11-L33
     Ok(Some(message.to_string()))
 }
