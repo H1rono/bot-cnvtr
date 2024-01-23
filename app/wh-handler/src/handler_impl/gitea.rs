11176087eacd15a2a11c93227b976477a672960b
H1rono <hronok66@gmail.com>
2024-01-24 03:56:01 +0900

    :sparkles: Impl some gitea events

--- app/wh-handler/src/handler_impl/gitea.rs
+++ app/wh-handler/src/handler_impl/gitea.rs
@@ -1,4 +1,4 @@
-use std::str::from_utf8;
+use std::{ops::Deref, str::from_utf8};
 
 use http::HeaderMap;
 use indoc::formatdoc;
@@ -16,7 +16,36 @@ pub(super) fn handle(headers: HeaderMap, payload: &str) -> Result<Option<String>
         .and_then(|v| from_utf8(v).map_err(|_| Error::WrongType))?;
     let message = match event_type {
         "create" => Some(create(from_str(payload)?)),
-        _ => default(event_type, from_str(payload)?),
+        "delete" => Some(delete(from_str(payload)?)),
+        "fork" => Some(fork(from_str(payload)?)),
+        "push" => Some(push(from_str(payload)?)),
+        "issues" => Some(issues(from_str(payload)?)),
+        "pull_request" => Some(pull_request(from_str(payload)?)),
+        "issue_assign"
+        | "issue_label"
+        | "issue_milestone"
+        | "issue_comment"
+        | "pull_request_assign"
+        | "pull_request_label"
+        | "pull_request_milestone"
+        | "pull_request_comment"
+        | "pull_request_review_approved"
+        | "pull_request_review_rejected"
+        | "pull_request_review_comment"
+        | "pull_request_sync"
+        | "pull_request_review_request"
+        | "wiki"
+        | "repository"
+        | "release"
+        | "package" => default(event_type, from_str(payload)?),
+        _ => {
+            // TODO: tracing
+            eprintln!(
+                "received unexpected header: `X-Gitea-Event: {}`",
+                event_type
+            );
+            return Err(Error::WrongType);
+        }
     };
     let Some(message) = message else {
         return Ok(None);
@@ -35,21 +64,111 @@ fn create(payload: th::CreatePayload) -> String {
     } = &payload;
     formatdoc! {
         r##"
-            [{}] {} `{}` was created by {}.
+            [{}] {} `{}` was created by {}
         "##,
         repo_str(repo), ref_type, r#ref, user_str(sender)
     }
 }
 
-/// X-Gitea-Event: *
-fn default(event_type: &str, _payload: Value) -> Option<String> {
-    Some(formatdoc! {
+/// X-Gitea-Event: delete
+fn delete(payload: th::DeletePayload) -> String {
+    let th::DeletePayload {
+        r#ref,
+        ref_type,
+        repo,
+        sender,
+        ..
+    } = &payload;
+    formatdoc! {
         r#"
-            GiteaからWebhookが送信されました。 イベント: {}
-            実装は現在工事中です :constructon:
+            [{}] {} `{}` was deleted by {}
         "#,
-        event_type
-    })
+        repo_str(repo), ref_type, r#ref, user_str(sender)
+    }
+}
+
+/// X-Gitea-Event: fork
+fn fork(payload: th::ForkPayload) -> String {
+    let th::ForkPayload {
+        forkee,
+        repo,
+        sender,
+    } = &payload;
+    formatdoc! {
+        r#"
+            [{}] forked to {} by {}
+        "#,
+        repo_str(repo), repo_str(forkee), user_str(sender)
+    }
+}
+
+/// X-Gitea-Event: push
+fn push(payload: th::PushPayload) -> String {
+    let th::PushPayload {
+        r#ref,
+        commits,
+        repo,
+        sender,
+        ..
+    } = &payload;
+    let commit_count = commits.len();
+    let commit_unit = if commit_count == 1 { "" } else { "s" };
+    let commits = commits
+        .iter()
+        .map(|c| {
+            let th::PayloadCommit {
+                id, message, url, ..
+            } = c.deref();
+            format!("[`{}`]({}) {}", &id[0..7], url, message)
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+    formatdoc! {
+        r#"
+            [{}:{}] {} commit{} was pushed by {}
+            {}
+        "#,
+        repo_str(repo), r#ref, commit_count, commit_unit, user_str(sender), commits
+    }
+}
+
+/// X-Gitea-Event: issues
+fn issues(payload: th::IssuePayload) -> String {
+    let th::IssuePayload {
+        action,
+        index,
+        issue,
+        repository: repo,
+        sender,
+        ..
+    } = &payload;
+    formatdoc! {
+        r#"
+            [{}] issue [#{} {}]({}) {} by {}
+        "#,
+        repo_str(repo), index, &issue.title, &issue.html_url, action, user_str(sender)
+    }
+}
+
+fn pull_request(payload: th::PullRequestPayload) -> String {
+    let th::PullRequestPayload {
+        action,
+        pull_request,
+        repository: repo,
+        sender,
+        ..
+    } = &payload;
+    formatdoc! {
+        r#"
+            [{}] Pull Request {} {} by {}
+        "#,
+        repo_str(repo), pr_str(pull_request), action, user_str(sender)
+    }
+}
+
+/// X-Gitea-Event: *
+fn default(_event_type: &str, _payload: Value) -> Option<String> {
+    None
 }
 
 fn repo_str(repo: &th::Repository) -> String {
@@ -58,4 +177,14 @@ fn repo_str(repo: &th::Repository) -> String {
 
 fn user_str(user: &th::User) -> String {
     format!("[{}]({})", user.user_name, user.avatar_url)
+}
+
+fn pr_str(pr: &th::PullRequest) -> String {
+    let th::PullRequest {
+        id,
+        title,
+        html_url,
+        ..
+    } = pr;
+    format!("[#{} {}]({})", id, title, html_url)
 }
