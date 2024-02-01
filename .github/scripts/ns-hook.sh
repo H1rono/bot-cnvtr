ffb15fd7418ae3e78d649f7aa2bc5b7a4617a7c3
H1rono <hronok66@gmail.com>
2024-02-01 14:21:44 +0900

    :sparkles: Notify neoshowcase ghcr build

---
+++ .github/scripts/ns-hook.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+function gh_api() {
+	gh api \
+		-H "Accept: application/vnd.github+json" \
+		-H "X-GitHub-Api-Version: 2022-11-28" \
+		"$@"
+}
+
+function hook_id() {
+	gh_api "/repos/${REPOSITORY}/hooks" \
+		| jq -r 'map(select(.type == "Repository")) | .[0].id'
+}
+
+function push_delivery_id() {
+	gh_api "/repos/${REPOSITORY}/hooks/${HOOK_ID}/deliveries" \
+		| jq -r 'map(select(.event == "push")) | .[0].id'
+}
+
+function delivery_request() {
+	gh_api "/repos/${REPOSITORY}/hooks/${HOOK_ID}/deliveries/${DELIVERY_ID}" \
+		| jq '.request'
+}
+
+function request() {
+	json=`cat`
+	headers=`echo "$json" | jq '.headeers'`
+	payload=`echo "$json" | jq -r '.payload | @text'`
+
+	content_type=`echo "$headers" | jq -r '."Content-Type"'`
+	github_delivery=`echo "$headers" | jq -r '."X-GitHub-Delivery"'`
+	github_event=`echo "$headers" | jq -r '."X-GitHub-Event"'`
+	github_hook_id=`echo "$headers" | jq '."X-GitHub-Hook-ID"'`
+
+	curl -f -X POST \
+		-H "Content-Type: $content_type" \
+		-H "X-GitHub-Delivery: $github_delivery" \
+		-H "X-GitHub-Event: $github_event" \
+		-H "X-GitHub-Hook-ID: $github_hook_id" \
+		-d "$payload" \
+		"${1}"
+}
+
+case "${1}" in
+	"hook_id" | "hook-id" )
+		hook_id ;;
+	"push_delivery_id" | "push-delivery-id" )
+		push_delivery_id ;;
+	"delivery_request" | "delivery-request" )
+		delivery_request ;;
+	"request" )
+		request "${2}" ;;
+	* ) exit 1 ;;
+esac
