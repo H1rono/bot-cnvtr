b30984e9ad6331aba042afdb1d411a5f79d3eefb
H1rono <hronok66@gmail.com>
2024-05-19 22:42:11 +0900

    :arrow_up: Bump rust to 1.78

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
 
       toolchain = pkgs.fenix.fromToolchainFile {
         file = ./rust-toolchain.toml;
-        sha256 = "sha256-7QfkHty6hSrgNM0fspycYkRcB82eEqYa4CoAJ9qA3tU=";
+        sha256 = "sha256-opUgs6ckUQCyDxcB9Wy51pqhd0MPGHUVbwRKKPGiwZU=";
       };
       craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
 
