4215603269346b4d414ac34750bb2973f311c727
H1rono <hronok66@gmail.com>
2024-09-30 05:41:25 +0900

    :fire: Remove `parse_col_str2uuid`

--- infra/repository/src/model.rs
+++ infra/repository/src/model.rs
@@ -1,5 +1,3 @@
-use sqlx::Row;
-
 mod group;
 mod group_member;
 mod owner;
@@ -11,15 +9,3 @@ pub use group_member::GroupMember;
 pub use owner::Owner;
 pub use user::User;
 pub use webhook::Webhook;
-
-fn parse_col_str2uuid<Id>(row: &sqlx::mysql::MySqlRow, col: &str) -> sqlx::Result<Id>
-where
-    Id: From<uuid::Uuid>,
-{
-    let id_str = row.try_get(col)?;
-    let id = uuid::Uuid::parse_str(id_str).map_err(|e| sqlx::Error::ColumnDecode {
-        index: col.to_string(),
-        source: e.into(),
-    })?;
-    Ok(id.into())
-}
