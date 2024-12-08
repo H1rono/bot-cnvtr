d40593d4080ca4c4d71f960ccf98c5a7a4427a30
H1rono <hronok66@gmail.com>
2024-12-09 04:22:55 +0900

    :sparkles: Update traits

--- usecases/src/app.rs
+++ usecases/src/app.rs
@@ -3,10 +3,23 @@ use domain::Infra;
 use crate::{Bot, WebhookHandler};
 
 #[must_use]
-pub trait App<I: Infra>: Send + Sync + 'static {
+pub trait App<I: Infra> {
     type Bot: Bot<I>;
     type WebhookHandler: WebhookHandler<I>;
 
-    fn bot(&self) -> &Self::Bot;
-    fn webhook_handler(&self) -> &Self::WebhookHandler;
+    fn split(self) -> (Self::Bot, Self::WebhookHandler);
+}
+
+impl<I, B, WH> App<I> for (B, WH)
+where
+    I: Infra,
+    B: Bot<I>,
+    WH: WebhookHandler<I>,
+{
+    type Bot = B;
+    type WebhookHandler = WH;
+
+    fn split(self) -> (Self::Bot, Self::WebhookHandler) {
+        self
+    }
 }
--- usecases/src/bot.rs
+++ usecases/src/bot.rs
@@ -1,10 +1,19 @@
-use std::future::Future;
+use std::sync::Arc;
 
-use traq_bot_http::Event;
+use http::{Request, Response};
+use http_body::Body;
+use tower::util::BoxCloneService;
 
-use domain::{Infra, Result};
+use domain::Infra;
 
 #[must_use]
-pub trait Bot<I: Infra>: Send + Sync + 'static {
-    fn handle_event(&self, infra: &I, event: Event) -> impl Future<Output = Result<()>> + Send;
+pub trait Bot<I: Infra> {
+    fn build_service<B>(
+        self,
+        infra: Arc<I>,
+    ) -> BoxCloneService<Request<B>, Response<String>, domain::Error>
+    where
+        B: Body + Send + 'static,
+        B::Data: Send + 'static,
+        B::Error: Into<Box<dyn std::error::Error + Send + Sync + 'static>>;
 }
