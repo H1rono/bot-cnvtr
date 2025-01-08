494e398c069074ca753c950da3d7898fb0102383
H1rono <hronok66@gmail.com>
2025-01-09 03:21:11 +0900

    :adhesive_bandage: Fix

--- cron/src/notifier.rs
+++ cron/src/notifier.rs
@@ -1,4 +1,6 @@
-use std::{error::Error as StdError, sync::Arc, time::Duration};
+use std::error::Error as StdError;
+use std::sync::Arc;
+use std::time::Duration;
 
 use tokio::time::interval;
 use tokio_stream::{wrappers::UnboundedReceiverStream, Stream, StreamExt};
@@ -29,7 +31,7 @@ async fn send_events(infra: &impl Infra, events: &[Event]) {
         tracing::info!("sending {} events...", events.len());
     }
     for event in events {
-        tracing::debug!(event_kind = ?event.kind(), channel_id = ?event.channel_id());
+        tracing::info!(event_kind = event.kind(), channel_id = %event.channel_id());
         let res = infra
             .traq_client()
             .send_message(event.channel_id(), &event.body(), false)
@@ -44,8 +46,9 @@ impl Notifier {
     /// never returns
     #[tracing::instrument(skip_all, fields(period = period.as_millis()))]
     pub async fn run(self, infra: Arc<impl Infra>, period: Duration) {
+        let Self(rx) = self;
         let interval = interval(period);
-        let mut recv_stream = UnboundedReceiverStream::new(self.0).timeout_repeating(interval);
+        let mut recv_stream = UnboundedReceiverStream::new(rx).timeout_repeating(interval);
         loop {
             tracing::trace!("tick");
             let event_stream = (&mut recv_stream).map_while(Result::ok);
